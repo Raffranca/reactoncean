aa5e81640e33af92c950654294fb2ffaa6d72190
rafae <rafaelafrancamedeiros@gmail.com>
2021-02-19 23:52:14 -0300

    Projeto jogo da velha

---
+++ reactocean/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Board() {
+        return (
+        <div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+        </div>
+    );
+}
+
+// Criação do componente Game
+function Game() {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board/>
+            </div>
+            <div className="game-info">
+                Info
+            </div>
+        </div>
+    );
+}
+ReactDOM.render(
+    // Elemento que será renderizado
+    <Game/>,
+    // Local em que esse elemento será renderizado
+    document.getElementById('root')
+);
